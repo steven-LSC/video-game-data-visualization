0da062debafc4b7ea35446f355c7386252279161
Steven Lin <steven123890@gmail.com>
2025-04-01 12:01:31 +0900

    add vercel.json 嘗試解決路徑問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Sales from "./components/Sales/Sales";
 import AggressionAnalysis from "./components/AggressionAnalysis/AggressionAnalysis";
@@ -9,6 +14,7 @@ function App() {
       <div className="app-container">
         <Header />
         <Routes>
+          <Route path="/" element={<Navigate to="/sales" replace />} />
           <Route path="/sales" element={<Sales />} />
           <Route path="/aggression-analysis" element={<AggressionAnalysis />} />
         </Routes>
